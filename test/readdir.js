6a895bd52b7910b41a5d031586d5ce94ba4efc58
Thorsten Lorenz <thlorenz@gmx.de>
2012-06-03 16:50:57 -0400

    erroring out when negated and non negated globs are mixed

--- test/readdir.js
+++ test/readdir.js
@@ -1,5 +1,6 @@
 /*jshint asi:true */
 
+
 var path  = require('path') 
   , fsrec = require('../index.js')
   , root  = path.join(__dirname, '../test/bed')
@@ -45,14 +46,14 @@ describe('reading root', function () {
     describe('without filters', function () {
 
         it('gets all directories', function (done) {
-            fsrec.readdir(opts(), function (res) {
-                res.directories.length.should.equal(totalDirs);
+            fsrec.readdir(opts(), function (err, res) {
+                res.directories.should.have.length(totalDirs);
                 done();
             })
         })
         it('gets all files', function (done) {
-            fsrec.readdir(opts(), function (res) {
-                res.files.length.should.equal(totalFiles);
+            fsrec.readdir(opts(), function (err, res) {
+                res.files.should.have.length(totalFiles);
                 done();
             })
         })
@@ -62,29 +63,29 @@ describe('reading root', function () {
 
         describe('normal', function () {
             it('"*.ext1"', function (done) {
-                fsrec.readdir(opts( { fileFilter: '*.ext1' } ), function (res) {
-                    res.files.length.should.equal(ext1Files);
+                fsrec.readdir(opts( { fileFilter: '*.ext1' } ), function (err, res) {
+                    res.files.should.have.length(ext1Files);
                     done();
                 })
             })
 
             it('["*.ext1", "*.ext3"]', function (done) {
-                fsrec.readdir(opts( { fileFilter: [ '*.ext1', '*.ext3' ] } ), function (res) {
-                    res.files.length.should.equal(ext1Files + ext3Files);
+                fsrec.readdir(opts( { fileFilter: [ '*.ext1', '*.ext3' ] } ), function (err, res) {
+                    res.files.should.have.length(ext1Files + ext3Files);
                     done();
                 })
             })
 
             it('"root_dir1"', function (done) {
-                fsrec.readdir(opts( { directoryFilter: 'root_dir1' }), function(res) {
-                    res.directories.length.should.equal(1);
+                fsrec.readdir(opts( { directoryFilter: 'root_dir1' }), function (err, res) {
+                    res.directories.should.have.length(1);
                     done();
                 })
             })
 
             it('["root_dir1", "*dir1_subdir1"]', function (done) {
-                fsrec.readdir(opts( { directoryFilter: [ 'root_dir1', '*dir1_subdir1' ]}), function(res) {
-                    res.directories.length.should.equal(2);
+                fsrec.readdir(opts( { directoryFilter: [ 'root_dir1', '*dir1_subdir1' ]}), function (err, res) {
+                    res.directories.should.have.length(2);
                     done();
                 })
             })
@@ -93,19 +94,30 @@ describe('reading root', function () {
 
         describe('negated', function () {
             it('"!*.ext1"', function (done) {
-                fsrec.readdir(opts( { fileFilter: '!*.ext1' } ), function (res) {
-                    res.files.length.should.equal(totalFiles - ext1Files);
+                fsrec.readdir(opts( { fileFilter: '!*.ext1' } ), function (err, res) {
+                    res.files.should.have.length(totalFiles - ext1Files);
                     done();
                 })
             })
+
             it('["!*.ext1", "!*.ext3"]', function (done) {
-                fsrec.readdir(opts( { fileFilter: [ '!*.ext1', '!*.ext3' ] } ), function (res) {
-                    res.files.length.should.equal(ext2Files);
+                fsrec.readdir(opts( { fileFilter: [ '!*.ext1', '!*.ext3' ] } ), function (err, res) {
+                    res.files.should.have.length(ext2Files);
                     done();
                 })
             })
-            
         })
+
+        /* problematic when using mocha watcher, passes otherwise
+        describe('normal mixed with negated', function () {
+            it('["*.ext1", "!*.ext3"] throws error', function (done) {
+                fsrec.readdir(opts( { fileFilter: [ '*.ext1', '!*.ext3' ] } ), function (err, res) {
+                    err[0].toString().should.include('Cannot mix negated with non negated glob filters'); 
+                    done();
+                })
+            })
+        })
+        */
     })
 })
 
