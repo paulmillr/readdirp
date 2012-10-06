bd39d00078d27c5ee41cf6b6d5cbadf6cbbb7a10
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-05 22:35:38 -0400

    working simple grep example

--- examples/grep.js
+++ examples/grep.js
@@ -1,10 +1,73 @@
+'use strict';
 var readdirp =  require('..')
+  , util     =  require('util')
+  , fs       =  require('fs')
   , path     =  require('path')
+  , Stream   =  require('stream')
   , tap      =  require('tap-stream')
-  , util     =  require('util');
+  , es       =  require('event-stream')
+  ;
 
-readdirp({ root: path.join(__dirname + '/../test/bed'), fileFilter: '*'})
-  .pipe(tap(0));
+function findLinesMatching (searchTerm) {
 
+  return es.through(function (entry) {
+    var lineno = 0
+      , matchingLines = []
+      , fileStream = this;
 
+    function filter () {
+      return es.mapSync(function (line) {
+        lineno++;
+        return ~line.indexOf(searchTerm) ? lineno + ': ' + line : undefined;
+      });
+    }
 
+    function aggregate () {
+      return es.through(
+          function write (data) { 
+            matchingLines.push(data); 
+          }
+        , function end () {
+
+            // drop files that had no matches
+            if (matchingLines.length) {
+              var result = { file: entry, lines: matchingLines };
+
+              // pass result on to file stream
+              fileStream.emit('data', result);
+            }
+            this.emit('end');
+          }
+      );
+    }
+
+    fs.createReadStream(entry.fullPath, { encoding: 'utf-8' })
+
+      // handle file contents line by line
+      .pipe(es.split('\n'))
+
+      // keep only the lines that matched the term
+      .pipe(filter())
+
+      // aggregate all matching lines and delegate control back to the file stream
+      .pipe(aggregate())
+      ;
+  });
+}
+
+console.log('grepping for "arguments"');
+
+// create a stream of all javascript files found in this and all sub directories
+readdirp({ root: path.join(__dirname), fileFilter: '*.js' })
+
+  // find all lines matching the term for each file (if none found, that file is ignored)
+  .pipe(findLinesMatching('arguments'))
+
+  // format the results and output
+  .pipe(
+    es.mapSync(function (res) {
+      return '\n\n' + res.file.path + '\n\t' + res.lines.join('\n\t');
+    })
+  )
+  .pipe(process.stdout)
+  ;
