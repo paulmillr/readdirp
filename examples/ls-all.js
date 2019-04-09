03c760f6e9af7b147686b4708d0f811f40d2e8b5
Paul Miller <paul@paulmillr.com>
2019-04-09 10:35:50 +0200

    Update example.

--- examples/ls-all.js
+++ examples/ls-all.js
@@ -5,9 +5,13 @@ const start = async (stream) => {
   for await (const chunk of stream) {
     // Check memory usage with this line. It should be 10MB or so.
     // Comment it out if you simply want to list files.
-    // await new Promise(resolve => setTimeout(resolve, 500));
-    i++;
-    // console.log(`${++i}: ${chunk.path}`);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    // i++;
+    if (i === 5) {
+      stream.pause();
+      setTimeout(() => {stream.resume()}, 1000);
+    }
+    console.log(`${++i}: ${chunk.path}`);
   }
   console.log('DONE', i);
 };
