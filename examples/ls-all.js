1034295518b58195cb61ecc1c7ab8e74db2f33c3
Paul Miller <paul@paulmillr.com>
2019-04-09 07:00:09 +0300

    FIX STREAM by adding isPaused() check.

--- examples/ls-all.js
+++ examples/ls-all.js
@@ -1,16 +1,15 @@
-#!/usr/bin/env node
-const ReaddirpStream = require('../lib/stream');
+const readdirp = require('..');
 
 const start = async (stream) => {
   let i = 0;
   for await (const chunk of stream) {
     // Check memory usage with this line. It should be 10MB or so.
     // Comment it out if you simply want to list files.
-    await new Promise(resolve => setTimeout(resolve, 500));
-
-    console.log(`${++i}: ${chunk.path}`);
+    // await new Promise(resolve => setTimeout(resolve, 500));
+    i++;
+    // console.log(`${++i}: ${chunk.path}`);
   }
   console.log('DONE', i);
 };
 
-start(new ReaddirpStream({root: '../..', entryType: 'all'}));
+start(readdirp('../..', {entryType: 'all'}));
