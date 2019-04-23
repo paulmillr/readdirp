0622b542a8d4ec038571ec24f4ecdb6bd81bab65
Artem Kobzar <dendishiz0@gmail.com>
2019-04-23 18:04:12 +0300

    chore: Remove unnecessary _ended field.

--- index.js
+++ index.js
@@ -82,7 +82,6 @@ class ReaddirpStream extends Readable {
     const opts = {...ReaddirpStream.defaultOptions, ...options};
     const {root} = opts;
 
-    this._ended = false;
     this._fileFilter = normalizeFilter(opts.fileFilter);
     this._directoryFilter = normalizeFilter(opts.directoryFilter);
     this._stat = opts.lstat ? lstat : stat;
@@ -182,10 +181,13 @@ class ReaddirpStream extends Readable {
     this._endStreamIfQueueIsEmpty();
   }
 
+  _isStreamShouldBeEnded() {
+    return this.parents.length === 0 && this.filesToRead === 0 && this.readable;
+  }
+
   _endStreamIfQueueIsEmpty() {
-    if (this.parents.length === 0 && this.filesToRead === 0 && this.readable) {
+    if (this._isStreamShouldBeEnded()) {
       this.push(null);
-      this._ended = true;
     }
   }
 
