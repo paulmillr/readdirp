71712595630940de4fbceec7405ad844b1a190e7
Robert Kieffer <robert@broofa.com>
2019-11-21 23:00:08 -0800

    reset list.js to upstream

--- examples/list.js
+++ examples/list.js
@@ -8,14 +8,18 @@ const read = async (directory) => {
   const stream = readdirp(directory, {type: 'all'});
   let i = 0;
   const start = Date.now();
-
-  stream.on('data', chunk => {
+  for await (const chunk of stream) {
     i++;
-  });
+    // Check memory usage with this line. It should be 10MB or so.
+    // Comment it out if you simply want to list files.
+    // await new Promise(resolve => setTimeout(resolve, 500));
+    // if (i % 100000 === 0)
+      // console.log(`${i}`, chunk);
+  }
+  console.log('finished', i, 'files in', Date.now() - start, 'ms');
 
-  stream.on('end', chunk => {
-    console.log('finished', i, 'files in', Date.now() - start, 'ms');
-  });
+  // const entries = await readdirp.promise(directory, {alwaysStat: false});
+  // console.log('Promise done', entries.length);
 };
 
 read('../..');
