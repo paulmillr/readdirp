bd865e5f164a4784cceb7484c286f1ef23332b27
Paul Miller <paul@paulmillr.com>
2019-11-24 18:32:23 +0200

    Add benchmark for mem and cpu usage.

---
+++ examples/bench.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+
+'use strict';
+
+const readdirp = require('..');
+
+function logMem(i) {
+  const vals = Object.entries(process.memoryUsage()).map(([k, v]) => {
+    return `${k}=${(`${(v/1e6).toFixed(1)}M`).padEnd(7)}`;
+  });
+  console.log(String(i).padStart(6), ...vals);
+}
+
+const read = async (directory) => {
+  const stream = readdirp(directory, {type: 'all'});
+  let i = 0;
+  const start = Date.now();
+  let lap = 0;
+
+  for await (const chunk of stream) {
+    if (i % 1000 == 0) {
+      const now = Date.now();
+      if (now - lap > 500) {
+        lap = now;
+        logMem(i);
+      }
+    }
+    i++;
+  }
+  logMem(i);
+
+  console.log(`Processed ${i} files in ${Date.now() - start} msecs`);
+};
+
+read('../..');
+
