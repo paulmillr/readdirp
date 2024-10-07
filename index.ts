62591086578e247e8b4220a6584d140535f70e56
Bengt Weiße <bengtler@gmail.com>
2024-10-07 12:09:41 +0200

    style: lint

--- index.ts
+++ index.ts
@@ -49,13 +49,34 @@ const defaultOptions: ReaddirpOptions = {
   alwaysStat: false,
   highWaterMark: 4096,
 };
-Object.freeze(defaultOptions)
+Object.freeze(defaultOptions);
 
 const RECURSIVE_ERROR_CODE = 'READDIRP_RECURSIVE_ERROR';
 const NORMAL_FLOW_ERRORS = new Set(['ENOENT', 'EPERM', 'EACCES', 'ELOOP', RECURSIVE_ERROR_CODE]);
-const ALL_TYPES = [EntryTypes.DIR_TYPE, EntryTypes.EVERYTHING_TYPE, EntryTypes.FILE_DIR_TYPE, EntryTypes.FILE_TYPE, 'files', 'directories', 'files_directories', 'all'];
-const DIR_TYPES = new Set([EntryTypes.DIR_TYPE, EntryTypes.EVERYTHING_TYPE, EntryTypes.FILE_DIR_TYPE, 'directories', 'files_directories']);
-const FILE_TYPES = new Set([EntryTypes.EVERYTHING_TYPE, EntryTypes.FILE_DIR_TYPE, EntryTypes.FILE_TYPE, 'files', 'files_directories']);
+const ALL_TYPES = [
+  EntryTypes.DIR_TYPE,
+  EntryTypes.EVERYTHING_TYPE,
+  EntryTypes.FILE_DIR_TYPE,
+  EntryTypes.FILE_TYPE,
+  'files',
+  'directories',
+  'files_directories',
+  'all',
+];
+const DIR_TYPES = new Set([
+  EntryTypes.DIR_TYPE,
+  EntryTypes.EVERYTHING_TYPE,
+  EntryTypes.FILE_DIR_TYPE,
+  'directories',
+  'files_directories',
+]);
+const FILE_TYPES = new Set([
+  EntryTypes.EVERYTHING_TYPE,
+  EntryTypes.FILE_DIR_TYPE,
+  EntryTypes.FILE_TYPE,
+  'files',
+  'files_directories',
+]);
 
 const isNormalFlowError = (error: any) => NORMAL_FLOW_ERRORS.has(error.code);
 const wantBigintFsStats = process.platform === 'win32';
